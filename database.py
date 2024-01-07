2cc235f2d772092941dff44e40b774e123ff92fe
qzzloz <xlivsxd@gmail.com>
2024-01-07 17:28:01 +0900

    파이보 프로젝트 최초 커밋

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# autocommit이 false: db.add(q)하고 db.commit() << 이거 안 하면 디비에 등록X
+
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)    # null값 허용x
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id = Column(Integer, primary_key=True)
+    create_date = Column(DateTime, nullable=False)
+    question_id = Column(Integer, ForeignKey("question.id"))
+    question = relationship("Question", backref="answers")  # 파라미터 순서대로: 참조할 모델명, 역참조설정(질문에서 답변을 거꾸로 참조)
